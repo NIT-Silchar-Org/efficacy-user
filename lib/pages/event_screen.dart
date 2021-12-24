24bb5f6cefe46734e1c50858a7b86ef41f6da757
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-12-25 01:39:04 +0530

    small changes

--- lib/pages/event_screen.dart
+++ lib/pages/event_screen.dart
@@ -36,7 +36,7 @@ class _EventScreenState extends State<EventScreen> {
       ),
       bottomSheet: SolidBottomSheet(
         showOnAppear: true,
-        canUserSwipe: false,
+        // canUserSwipe: false,
         maxHeight: MediaQuery.of(context).size.height - 300,
         headerBar: const SizedBox(
           height: 20,
@@ -47,6 +47,7 @@ class _EventScreenState extends State<EventScreen> {
         body: Padding(
           padding: const EdgeInsets.fromLTRB(35, 0, 35, 0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -203,7 +204,7 @@ class _EventScreenState extends State<EventScreen> {
                               textStyle: TextStyle(
                                 color: const Color(0xff191C1D).withOpacity(0.5),
                                 fontSize: 8.0,
-                                fontWeight: FontWeight.w500,
+                                fontWeight: FontWeight.w400,
                                 letterSpacing: 0.5,
                               ),
                             ),
@@ -254,7 +255,7 @@ class _EventScreenState extends State<EventScreen> {
                               textStyle: TextStyle(
                                 color: const Color(0xff191C1D).withOpacity(0.5),
                                 fontSize: 8.0,
-                                fontWeight: FontWeight.w500,
+                                fontWeight: FontWeight.w400,
                                 letterSpacing: 0.5,
                               ),
                             ),
@@ -264,7 +265,223 @@ class _EventScreenState extends State<EventScreen> {
                     ),
                   ],
                 ),
-              )
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                width: 114,
+                height: 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: const Color(0xffDAE5FF),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Icon(
+                      Icons.event_note_outlined,
+                      size: 10,
+                      color: Color(0xff213F8D),
+                    ),
+                    Text(
+                      "Add to Calender",
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                          color: Color(0xff213F8D),
+                          fontSize: 10.0,
+                          fontWeight: FontWeight.w300,
+                          letterSpacing: 0.5,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: Text(
+                  "Event Details",
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      color: const Color(0xff191C1D).withOpacity(0.7),
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 29),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Card(
+                      shadowColor: AppColorLight.cardShadow,
+                      elevation: 1,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: SizedBox(
+                        width: 125,
+                        height: 28,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              width: 22,
+                              height: 22,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/google.png'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "Google Form",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  color: Color(0xFFFECC07),
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0.5,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shadowColor: AppColorLight.cardShadow,
+                      elevation: 1,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: SizedBox(
+                        width: 125,
+                        height: 28,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Icon(
+                              Icons.facebook,
+                              size: 22,
+                              color: Color(0xff1877F2),
+                            ),
+                            Text(
+                              "Facebook",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  color: Color(0xff213f8d),
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0.5,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 21),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.person,
+                      size: 32,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Random Name",
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: const Color(0xff191C1D).withOpacity(0.7),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "Moderator",
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: const Color(0xff191C1D).withOpacity(0.5),
+                                fontSize: 8.0,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 36,
+                      height: 36,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: const Color(0xFFFFF0B6)),
+                      child: const Center(
+                        child: Icon(
+                          Icons.call_outlined,
+                          size: 14,
+                          color: Color(0xffFECC07),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "9876543210",
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: const Color(0xff191C1D).withOpacity(0.7),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Container(
+              //   margin: const EdgeInsets.only(top: 12),
+              //   child: Text(
+              //     "published on 12 March,2021",
+              //     style: GoogleFonts.poppins(
+              //       textStyle: TextStyle(
+              //           color: const Color(0xff191C1D).withOpacity(0.7),
+              //           fontSize: 8.0,
+              //           fontWeight: FontWeight.w300,
+              //           letterSpacing: 0.5,
+              //           fontStyle: FontStyle.italic),
+              //     ),
+              //   ),
+              // )
             ],
           ),
         ),
