256b6bceb8d8113a5e916f46f3c3a398f01d0493
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-12-26 10:06:11 +0530

    final changes

--- lib/pages/event_screen.dart
+++ lib/pages/event_screen.dart
@@ -13,6 +13,8 @@ class EventScreen extends StatefulWidget {
 }
 
 class _EventScreenState extends State<EventScreen> {
+  final String description =
+      "Flutter is Google’s mobile UI framework for crafting high-quality native interfaces on iOS and Android in record time. Flutter works with existing code, is used by developers and organizations around the world, and is free and open source.";
   int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
@@ -316,6 +318,7 @@ class _EventScreenState extends State<EventScreen> {
                     ),
                   ),
                 ),
+                DetailsWidget(text: description),
                 Container(
                   margin: const EdgeInsets.only(top: 29),
                   child: Row(
@@ -545,6 +548,91 @@ class _EventScreenState extends State<EventScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class DetailsWidget extends StatefulWidget {
+  final String text;
+
+  const DetailsWidget({Key? key, required this.text}) : super(key: key);
+  @override
+  _DetailsWidgetState createState() => _DetailsWidgetState();
+}
+
+class _DetailsWidgetState extends State<DetailsWidget> {
+  String firstHalf = "";
+  String secondHalf = "";
+
+  bool flag = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.text.length > 80) {
+      firstHalf = widget.text.substring(0, 80);
+      secondHalf = widget.text.substring(80, widget.text.length);
+    } else {
+      firstHalf = widget.text;
+      secondHalf = "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: secondHalf.isEmpty
+          ? Text(
+              firstHalf,
+              style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+                  color: const Color(0xff191C1D).withOpacity(0.5),
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: 0.5,
+                ),
+              ),
+            )
+          : Column(
+              children: <Widget>[
+                Text(
+                  flag ? (firstHalf + "...") : (firstHalf + secondHalf),
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      color: const Color(0xff191C1D).withOpacity(0.5),
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w300,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        flag ? "show more" : "show less",
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                            color: Color(0xff213F8D),
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    setState(() {
+                      flag = !flag;
+                    });
+                  },
+                ),
+              ],
+            ),
     );
   }
 }
