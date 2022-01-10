4436f50bfd07e4ed98508eae494a652eacd85f30
ManashX <manashborah075@gmail.com>
2022-01-10 17:21:03 +0530

    Create account screen

---
+++ lib/pages/account_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class accountScreen extends StatefulWidget {
+  const accountScreen({ Key? key }) : super(key: key);
+
+  @override
+  _accountScreenState createState() => _accountScreenState();
+}
+
+class _accountScreenState extends State<accountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
