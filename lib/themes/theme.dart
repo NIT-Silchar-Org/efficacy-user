7177d0a4511a1840590e06f4407fbfb4177492c6
Sumit Prasad <prasadsumit1917@gmail.com>
2022-01-14 13:32:43 +0530

    Theme file updated

--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -8,6 +8,26 @@ ThemeData lightTheme = ThemeData(
   primaryColor: AppColorLight.primary,
   backgroundColor: AppColorLight.background,
   scaffoldBackgroundColor: AppColorLight.background,
+  appBarTheme: AppBarTheme(
+    backgroundColor: AppColorLight.background,
+    shadowColor: AppColorLight.background,
+    foregroundColor: AppColorLight.mainText,
+    elevation: 0,
+  ),
+  cardTheme: CardTheme(
+    margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+    shadowColor: AppColorLight.cardShadow,
+    elevation: 0,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+  ),
+  iconTheme: IconThemeData(color: AppColorLight.subtitleText),
+  primaryIconTheme: IconThemeData(color: AppColorLight.mainText),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    elevation: 2,
+    backgroundColor: AppColorLight.background,
+    selectedItemColor: AppColorLight.secondary,
+    unselectedItemColor: const Color(0xFF7D7D7D).withOpacity(0.5),
+  ),
   textTheme: TextTheme(
     headline1: GoogleFonts.poppins(
       textStyle: TextStyle(
