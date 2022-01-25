86fd46eef593edfabe0e859089c28973dd083051
san234751 <sankitkundu96@gmail.com>
2022-01-25 11:19:36 +0530

    models added

--- lib/provider/event_provider.dart
+++ lib/provider/event_provider.dart
@@ -1,6 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:efficacy_user/models/event_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-class EventProvider with ChangeNotifier{
-    EventModel _event; 
+
+class EventProvider with ChangeNotifier {
+  EventModel fetchEvent(String eventId) {
+    try {
+      final token = FirebaseAuth.instance.currentUser!.getIdToken();
+      final response = networkhandler().getevent('event/$eventId');
+      var details = json.decode(response.body)['data'];
+      EventModel event = EventModel.fromJson(details);
+      return event;
+    } catch (e) {
+      throw(e);
+    }
+  }
 }
