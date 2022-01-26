8ba454b07881409e764f0f544dac169e5aac1dcf
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-01-27 00:12:16 +0530

    Added model

--- lib/constant/endpoints.dart
+++ lib/constant/endpoints.dart
@@ -1,2 +1,3 @@
 const String getevent = '/event';
 const String likeunlikepost = '/like';
+const String getdev = '/dev';
---
+++ lib/provider/dev_provider.dart
@@ -0,0 +1,5 @@
+import 'package:efficacy_user/services/service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class DevProvider with ChangeNotifier {}
