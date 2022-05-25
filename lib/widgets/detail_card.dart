0293461fbc28b04baa78e3e788b5260490c21306
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-05-25 13:40:19 +0530

    Update detail_card.dart

--- lib/widgets/detail_card.dart
+++ lib/widgets/detail_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// ignore: must_be_immutable
 class DetailCard extends StatelessWidget {
   String text1;
   String? text2;
